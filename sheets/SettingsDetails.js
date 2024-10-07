b47716088386e6ca7cc2537b91e96fa6a15bb2bc
Eduardo Regis <eduardo.regis@mblabs.com.br>
2024-10-07 10:39:49 -0300

    slider subComponent with interaction with async storage

--- sheets/SettingsDetails.js
+++ sheets/SettingsDetails.js
@@ -1,17 +1,47 @@
 import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Colors } from "../constants/colors";
-import Slider from "@react-native-community/slider";
 import CustomButton from "../components/CustomButton";
 import { useAsyncStorage } from "../data/useAsyncStorage";
+import CustomSlider from "../components/CustomSlider";
 
 function SettingsDetails() {
   const asyncStorageHook = useAsyncStorage();
+  const [ambienceVolume, setAmbienceValue] = useState(0);
+  const [musicVolume, setMusicValue] = useState(0);
+  const [effectVolume, setEffectValue] = useState(0);
 
   async function resetGame() {
     await asyncStorageHook.setStorageHandler("@dialogue", 0);
     await asyncStorageHook.setStorageHandler("@archetype", 0);
     await asyncStorageHook.setStorageHandler("@herosJourney", 0);
+  }
+
+  function ambienceChange(progress) {
+    asyncStorageHook.setStorageHandler("@ambienceVolume", progress);
+    console.log(progress);
+  }
+
+  function musicChange(progress) {
+    asyncStorageHook.setStorageHandler("@musicVolume", progress);
+    console.log(progress);
+  }
+
+  function effectChange(progress) {
+    asyncStorageHook.setStorageHandler("@effectVolume", progress);
+    console.log(progress);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    setAmbienceValue(
+      await asyncStorageHook.getStorageHandler("@ambienceVolume")
+    );
+    setMusicValue(await asyncStorageHook.getStorageHandler("@musicVolume"));
+    setEffectValue(await asyncStorageHook.getStorageHandler("@effectVolume"));
   }
 
   return (
@@ -30,18 +60,21 @@ function SettingsDetails() {
       <View style={styles.container}>
         <View style={styles.contentContainer}>
           <View style={styles.slidersContainer}>
-            <View style={styles.sliderContainer}>
-              <Text style={styles.text}>Vol. de ambiente:</Text>
-              <Slider style={styles.slider} />
-            </View>
-            <View style={styles.sliderContainer}>
-              <Text style={styles.text}>Vol. de música:</Text>
-              <Slider style={styles.slider} />
-            </View>
-            <View style={styles.sliderContainer}>
-              <Text style={styles.text}>Vol. de efeitos:</Text>
-              <Slider style={styles.slider} />
-            </View>
+            <CustomSlider
+              text="Vol. de ambiente:"
+              onChange={ambienceChange}
+              value={ambienceVolume}
+            />
+            <CustomSlider
+              text="Vol. de música:"
+              onChange={musicChange}
+              value={musicVolume}
+            />
+            <CustomSlider
+              text="Vol. de efeitos:"
+              onChange={effectChange}
+              value={effectVolume}
+            />
           </View>
           <View style={styles.creditsAndRestartContainer}>
             <CustomButton title={"Reiniciar Aventura"} onPress={resetGame} />
@@ -86,23 +119,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     marginLeft: 30,
-  },
-  sliderContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    width: 343,
-    marginVertical: 12,
-  },
-  text: {
-    color: Colors.primary300,
-    fontSize: 21,
-    fontFamily: "macondo-regular",
-    textAlign: "center",
-  },
-  slider: {
-    width: 150,
-    height: 25,
   },
   creditsAndRestartContainer: {
     flex: 1,
